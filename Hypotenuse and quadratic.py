17ed22fe8a78cb1bb37e5a9f0fec0ed43d5ea7ba
Akshay <130158966+astro-lawliet@users.noreply.github.com>
2025-04-09 22:01:05 +0530

    Create Hypotenuse and quadratic.py

---
+++ Hypotenuse and quadratic.py
@@ -0,0 +1,20 @@
+#Pythagorus Theorum
+a = int(input('enter a: '))
+b = int(input('enter b: '))
+
+c = (a**2 + b**2)
+
+print(c)
+
+
+# Quadratic Formula
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+root1 = (-b + (b*b - 4*a*c)**0.5) / (2*a)
+root2 = (-b - (b*b - 4*a*c)**0.5) / (2*a)
+
+print(root1)
+print(root2)
